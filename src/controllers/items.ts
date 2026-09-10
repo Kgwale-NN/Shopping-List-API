14e0dddbb6c7887ce3a3e09fd8f1fc2b5528390e
Kgwale NN <kgoalemothiba8@gmail.com>
2026-09-10 11:30:14 +0200

    Add update and delete item controller functions

--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -1,22 +1,58 @@
-import {Item} from '../types/items'
+import { Item } from '../types/items'
 
-let items:Item[] = []
+let items: Item[] = []
 let currentId = 1
 
-export const addItem = (name:string,quantity:number,purchased:boolean) =>{
+export const addItem = (name: string, quantity: number, purchased: boolean) => {
 
-    const newItem = {id:currentId++,name,quantity,purchased}
+    const newItem = { id: currentId++, name, quantity, purchased }
     items.push(newItem)
     return newItem
 }
 
-export const getItems = ():Item[] => {
+export const getItems = (): Item[] => {
 
     return items
 }
 
-export const getItemById = (id:number):Item | undefined =>{
+export const getItemById = (id: number): Item | undefined => {
 
     const item = items.find((item) => item.id === id)
+
+    if (!item) {
+
+        return undefined
+    }
+
     return item
 }
+
+export const updateItem = (id: number, newName: string, newQuantity: number, newPurchased: boolean): Item | undefined => {
+
+    const item = items.find((Item) => Item.id === id)
+
+    if (!item) {
+
+        return undefined
+    }
+
+    item.name = newName
+    item.quantity = newQuantity
+    item.purchased = newPurchased
+
+    return item
+}
+
+export const deleteItem = (id:number): boolean =>{
+
+   const itemIndex = items.findIndex((item) => item.id === id)
+
+   if(itemIndex === -1){
+
+    return false
+   }
+
+   items.splice(itemIndex ,1)
+   return true
+}
+
