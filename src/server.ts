0764f4c826cce348c37a838265e16b8534aed1e9
Kgwale NN <kgoalemothiba8@gmail.com>
2026-09-11 15:32:45 +0200

    Connect item routes to the HTTP server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import * as http from "http"
 import type { IncomingMessage, ServerResponse } from "http"
+import {itemsRoutes} from './routes/items'
 
 const PORT = 2000
 
 const handleListener = (req:IncomingMessage, res:ServerResponse)=> {
 
-    res.writeHead(200 ,{"content-type" : "application/json"})
-    res.end(JSON.stringify({message : "Hello User"}))
+ 
 
+  if(req.url?.startsWith("/items")){
+
+    itemsRoutes(req,res)
+
+  }else{
+
+       res.writeHead(200 , {"content-type":"application/json"})
+       res.end(JSON.stringify({message : "Hello World"}))
+  }
 }
 
 const server = http.createServer(handleListener)
